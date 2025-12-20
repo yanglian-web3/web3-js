e0030b70dd6eab14c0e007814bb07dabc44af34f
Administrator <744890753@qq.com>
2025-12-20 16:02:06 +0800

    refactor(ethers function): 增加页面重置按钮
    - 在顶部下拉框右边添加页面重置按钮，点击该按钮清除页面数据（版本信息除外）

--- src/pages/ethers-function/index.tsx
+++ src/pages/ethers-function/index.tsx
@@ -12,27 +12,37 @@ export default function EthersFunction () {
     const [ethersVersion, setEthersVersion] = useState("6")
 
     /**
+     * 获取ethers版本对应的函数
+     * @param ethersVersion
+     * @returns
+     */
+    const getFunctionEvents = (ethersVersion:string) => {
+        const functionEvents = {
+            "6": {
+                connectWallet: connectWallet_v_six,
+                createWallet: createWallet_v_six
+            },
+            "5": {
+                connectWallet: connectWallet_v_five,
+                createWallet: createWallet_v_five
+            }
+        }
+        return functionEvents[ethersVersion]
+    }
+
+    /**
      * ethers版本切换
      * @param value
      */
     const ethersVersionChange = (value:string) => {
         setEthersVersion(value)
+        pageReset()
     }
+
     /**
-     * 创建钱包实例
+     * ethers版本6.16.0连接钱包
      */
-    const createWallet = () => {
-        const wallet = ethers.Wallet.createRandom()
-        return {
-            address: wallet.address,
-            privateKey: wallet.privateKey,
-            mnemonic: wallet.mnemonic.phrase
-        }
-    }
-    /**
-     * 连接钱包
-     */
-    const connectWallet = async () => {
+    const connectWallet_v_six = async () => {
         try {
             setConnectWalletAddress("")
             setConnectWalletLoading(true)
@@ -51,16 +61,73 @@ export default function EthersFunction () {
             setConnectWalletLoading(false)
         }
     }
+    /**
+     * ethers版本6.16.0创建钱包实例
+     */
+    const createWallet_v_six = () => {
+        const wallet = ethers.Wallet.createRandom()
+        return {
+            address: wallet.address,
+            privateKey: wallet.privateKey,
+            mnemonic: wallet.mnemonic.phrase
+        }
+    }
+
+    /**
+     * ethers版本5.7.2连接钱包
+     */
+    const connectWallet_v_five = async () => {
+        try {
+            setConnectWalletAddress("")
+            setConnectWalletLoading(true)
+            const provider = new ethers.providers.Web3Provider(window.ethereum)
+            await provider.send("eth_requestAccounts", [])
+            const signer = provider.getSigner();
+            const address = await signer.getAddress();
+            setConnectWalletAddress(address)
+            console.log("连接地址:", address);
+            console.log("Provider:", provider);
+            setConnectWalletLoading(false)
+        } catch (error){
+            console.log("连接钱包失败:", error);
+            setConnectWalletLoading(false)
+        }
+    }
+    /**
+     * ethers版本5.7.2创建钱包实例
+     */
+    const createWallet_v_five = () => {
+        const wallet = ethers.Wallet.createRandom()
+        return {
+            address: wallet.address,
+            privateKey: wallet.privateKey,
+            mnemonic: wallet.mnemonic.phrase
+        }
+    }
+    /**
+     * 连接钱包
+     */
+    const connectWallet = async () => {
+        getFunctionEvents(ethersVersion).connectWallet()
+    }
+
+    /**
+     * 页面重置
+     */
+    const pageReset = () => {
+        setConnectWalletAddress("")
+        setConnectWalletLoading(false)
+    }
 
     return <div className="max-w-4xl w-full flex-1 p-4 flex flex-wrap h-space-2 m-auto">
         <div className="w-full space-y-2">
-            <CyberCard>
+            <CyberCard contentClassName="flex justify-between items-center">
                 <Select value={ethersVersion} onValueChange={ethersVersionChange}>
                     <SelectGroup>
                         <div className="flex items-center gap-4"> {/* 横向布局 */}
                             <SelectLabel className="whitespace-nowrap text-md">ethers.js版本</SelectLabel>
                             <div className="flex-1"> {/* 让下拉框占满剩余空间 */}
-                                <SelectTrigger className="w-full text-cyber-blue-200">
+                                <SelectTrigger className="w-[160px] text-cyber-blue-200">
                                     <SelectValue placeholder="选择版本" />
                                 </SelectTrigger>
                             </div>
@@ -71,6 +138,7 @@ export default function EthersFunction () {
                         </SelectContent>
                     </SelectGroup>
                 </Select>
+                <CyberButton onClick={pageReset}>页面重置</CyberButton>
             </CyberCard>
             <CyberCard className="h-30">
                 <PageLoading loading={connectWalletLoading} size="mini">
